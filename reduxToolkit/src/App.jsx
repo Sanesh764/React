cd242e8ae72f057dce1db9e40d473d22410b9433
Sanesh <sanesh7644@gmail.com>
2026-07-09 22:36:57 +0530

    feat: implement AddTodo and Todos components, enhance state management with Redux

--- reduxToolkit/src/App.jsx
+++ reduxToolkit/src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>hello</h1>
+      <h1>learn redux toolkit here</h1>
+      <AddTodo/>
+      <Todos/>
     </>
   )
 }
