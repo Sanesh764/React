8bd7dac7c240db0cdc0970d5b1742e279a402ade
Sanesh <sanesh7644@gmail.com>
2026-07-11 07:14:43 +0530

    feat: add Login, Signup, and AddPost components; update index export

--- MegaBlog-1/frontend/src/components/index.js
+++ MegaBlog-1/frontend/src/components/index.js
@@ -3,10 +3,16 @@ import Header from "./header/Header";
 import Footer from "./footer/Footer";
 import Logo from "./Logo";
 import LogoutBtn from "./header/LogoutBtn";
+import Login from "../pages/Login";
+import SignUp from "../pages/Signup";
+import AddPost from "../pages/AddPost";
 
 export {
     Header,
     Footer,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Login,
+    SignUp,
+    AddPost,
 }
